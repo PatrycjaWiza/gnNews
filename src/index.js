38fc1c045603427916e89d9fc8bc777a97dfd797
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2023-03-29 21:34:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/gnNews/">
       <Provider store={store}>
         <App />
       </Provider>
